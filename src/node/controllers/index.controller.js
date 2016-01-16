53f5222205ecf9d435301a1a5947d50f8b41024c
mingram8 <mingram9473@gmail.com>
2016-01-16 20:59:19 +0000

    make it faster

--- src/node/controllers/index.controller.js
+++ src/node/controllers/index.controller.js
@@ -2,39 +2,51 @@ var cmd = require('child_process'),
     http = require('http'),
     request = require('request'),
     config = require('../../../config/config'),
-     YQL = require('yql');
+    YQL = require('yql');
 var min = 0;
 var max = 5;
 fs = require('fs');
 function actualDifference(string, string2) {
 
+    var string3 = ['', '']
+    if (string != '')
+    string =  string.replace(/\s+/g, '')
+    string2 = string2.replace(/\s+/g, '')
+
     if (string)
-        string3= string.split(string2)
+        string3 =string.split(string2)
+
 
     return string3[1]
 }
 var oldData = ''
-fs.watch(process.cwd()+'/words.log', function (event, filename) {
-    console.log('event is: ' + event);
+fs.watch(process.cwd() + '/words.log', function (event, filename) {
     var timer = new Date().getTime();
     fs.readFile(process.cwd() + '/words.log', 'utf8', function (err, data) {
         if (err) {
             return console.log(err);
         }
+
         var datas = actualDifference(data, oldData)
-        console.log("DATA:" + data)
-        var req = {}
-        req.body = {}
-        req.body.speech = datas;
         oldData = data;
         exports.sendVoice(datas)
 
     });
 });
-exports.sendVoice = function(voice) {
+
+
+var timer = new Date().getTime();
+var oldData2 = ''
+var newFlag = false;
+
+var time = new Date().getTime();
+
+exports.sendVoice = function (voice) {
+    console.log('voice ' + voice)
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-    request.post(config.host_ip+":"+config.host_port+'/sendSpeech', {speech: voice}, function (error, response, body) {
+    request.post(config.host_ip + ":" + config.host_port + '/sendSpeech', {form: {speech: voice}}, function (error, response, body) {
+        console.log(error)
         if (!error && response.statusCode == 200) {
         }
     })
